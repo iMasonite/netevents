5496c598439b9ce07bc658b28e9ded9a20199ebd
Michael Mason <mason1985@live.co.uk>
2016-06-01 17:56:43 +0100

    Pass some more info

--- src/main/java/com/zachsthings/netevents/ping/PingListener.java
+++ src/main/java/com/zachsthings/netevents/ping/PingListener.java
@@ -1,12 +1,8 @@
-/** Copyright (C) 2014 zml (netevents@zachsthings.com) Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the License. You may obtain
- * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable
- * law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- * for the specific language governing permissions and limitations under the License. */
 
 package com.zachsthings.netevents.ping;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -22,6 +18,13 @@ public class PingListener implements Listener {
   
   @EventHandler
   public void onPingEvent(PingEvent event) {
-    plugin.getLogger().info("Received ping from " + event.getHostname());
+    plugin.getLogger().info(String.format("Received ping from: %s - Host %s", event.getServerName(), event.getHostname()));
+    
+    for (Player player : plugin.getServer().getOnlinePlayers()) {
+      if (player.hasPermission("netevents.status")) {
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b > Ping event: &d" + event.getHostname()));
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b >> Server: &d" + event.getServerName()));
+      }
+    }
   }
 }
